2850b9e962cdc0ec59213b3a89d977872b42719a
Huang Changqian <634485702@qq.com>
2019-03-27 19:22:22 +0800

    提交的代码格式

--- SDK_C++/CodeCraft-2019/CodeCraft-2019.cpp
+++ SDK_C++/CodeCraft-2019/CodeCraft-2019.cpp
@@ -28,20 +28,20 @@ int main(int argc, char *argv[])
 {
     std::cout << "Begin" << std::endl;
 	
-	// if(argc < 5){
-	// 	std::cout << "please input args: carPath, roadPath, crossPath, answerPath" << std::endl;
-	// 	exit(1);
-	// }
+	if(argc < 5){
+		std::cout << "please input args: carPath, roadPath, crossPath, answerPath" << std::endl;
+		exit(1);
+	}
 	
-	// std::string carPath(argv[1]);
-	// std::string roadPath(argv[2]);
-	// std::string crossPath(argv[3]);
-	// std::string answerPath(argv[4]);
+	std::string carPath(argv[1]);
+	std::string roadPath(argv[2]);
+	std::string crossPath(argv[3]);
+	std::string answerPath(argv[4]);
 	
-	// std::cout << "carPath is " << carPath << std::endl;
-	// std::cout << "roadPath is " << roadPath << std::endl;
-	// std::cout << "crossPath is " << crossPath << std::endl;
-	// std::cout << "answerPath is " << answerPath << std::endl;
+	std::cout << "carPath is " << carPath << std::endl;
+	std::cout << "roadPath is " << roadPath << std::endl;
+	std::cout << "crossPath is " << crossPath << std::endl;
+	std::cout << "answerPath is " << answerPath << std::endl;
 
 	int car_num;
     int road_num;
@@ -50,12 +50,9 @@ int main(int argc, char *argv[])
     Cross *cross=NULL;
     Road *road=NULL;
 
-
-    get_imformation(&car_num, &cross_num, &road_num, &car, &cross, &road, CAR_PATH,CROSS_PATH,ROAD_PATH);
+    get_imformation(&car_num, &cross_num, &road_num, &car, &cross, &road, carPath.c_str(), crossPath.c_str(), roadPath.c_str());
 	project_car(car_num, cross_num, road_num, car, cross, road);
-	output_file(OUTPUT_PATH, car,  car_num);						
-
-	
+	output_file(answerPath.c_str(), car,  car_num);				
 
 	return 0;
 }
